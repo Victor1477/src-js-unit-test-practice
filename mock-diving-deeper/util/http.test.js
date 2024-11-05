57dc5c980ec0a81b1e07762e568426cbecc20d7d
Joao Victor Silva De Oliveira <joao.soliveira@telefonica.com>
2024-11-05 12:48:12 -0300

    refactor

--- mock-diving-deeper/util/http.test.js
+++ mock-diving-deeper/util/http.test.js
@@ -1,57 +1,61 @@
-import { expect, it, vi } from "vitest";
+import { beforeAll, describe, expect, it, vi } from "vitest";
 import { sendDataRequest } from "./http";
 import { HttpError } from "./errors";
 
-const requestData = { key: "content" };
-const responseData = { key2: "content2" };
+describe("sendDataRequest()", () => {
+  const requestData = { key: "content" };
+  const responseData = { key2: "content2" };
 
-const fetchMock = vi.fn((url, options) => {
-  return new Promise((resolve, reject) => {
-    if (typeof options.body !== "string") {
-      return reject("Not a string.");
-    }
-    resolve({
-      ok: true,
-      json: () => {
-        return new Promise((resolve, reject) => {
-          resolve(responseData);
-        });
-      },
-    });
-  });
-});
-
-vi.stubGlobal("fetch", fetchMock);
-
-it("Should return the correct available response data", async () => {
-  const response = await sendDataRequest(requestData);
-  return expect(response).toBe(responseData);
-});
-
-it("Should throw error if the request fails", () => {
-  fetchMock.mockImplementationOnce((url, options) => {
+  let fetchMock = vi.fn((url, options) => {
     return new Promise((resolve, reject) => {
+      if (typeof options.body !== "string") {
+        return reject("Not a string.");
+      }
       resolve({
-        ok: false,
-        json: () => {
+        ok: true,
+        json() {
           return new Promise((resolve, reject) => {
-            resolve();
+            resolve(responseData);
           });
         },
       });
     });
   });
-  return expect(sendDataRequest(requestData)).rejects.toBeInstanceOf(HttpError);
-});
 
-it("Should convert the body to a string before sending request", async () => {
-  let errorMessage;
+  beforeAll(() => {
+    vi.stubGlobal("fetch", fetchMock);
+  });
 
-  try {
-    await sendDataRequest(requestData);
-  } catch (e) {
-    errorMessage = e;
-  }
+  it("Should return the correct available response data", () => {
+    return expect(sendDataRequest(requestData)).resolves.toEqual(responseData);
+  });
 
-  expect(errorMessage).not.toBe("Not a string.");
+  it("Should throw error if the request fails", () => {
+    fetchMock.mockImplementationOnce((url, options) => {
+      return new Promise((resolve, reject) => {
+        resolve({
+          ok: false,
+          json() {
+            return new Promise((resolve, reject) => {
+              resolve(responseData);
+            });
+          },
+        });
+      });
+    });
+
+    return expect(sendDataRequest(requestData)).rejects.toBeInstanceOf(
+      HttpError
+    );
+  });
+
+  it("Should convert the body to a string before sending request", async () => {
+    let errorMessage;
+    try {
+      await sendDataRequest(requestData);
+    } catch (e) {
+      errorMessage = e;
+    }
+    expect(errorMessage).not.toBe("Not a string.");
+  });
 });
