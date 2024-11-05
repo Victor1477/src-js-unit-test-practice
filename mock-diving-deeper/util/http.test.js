11bc678b28357a38505ae37e570630b399d800af
Joao Victor Silva De Oliveira <joao.soliveira@telefonica.com>
2024-11-05 11:41:20 -0300

    3

--- mock-diving-deeper/util/http.test.js
+++ mock-diving-deeper/util/http.test.js
@@ -2,68 +2,49 @@ import { expect, it, vi } from "vitest";
 import { sendDataRequest } from "./http";
 import { HttpError } from "./errors";
 
-const responseData = "It Works!";
+const requestData = { key: "content" };
+const responseData = { key2: "content2" };
+
+const fetchMock = vi.fn((url, options) => {
+  return new Promise((resolve, reject) => {
+    if (typeof options.body !== "string") {
+      return reject("Not a string.");
+    }
+    resolve({
+      ok: true,
+      json: () => {
+        return new Promise((resolve, reject) => {
+          resolve(responseData);
+        });
+      },
+    });
+  });
+});
+
+vi.stubGlobal("fetch", fetchMock);
 
 it("Should return the correct available response data", async () => {
-  vi.stubGlobal(
-    "fetch",
-    vi.fn((url, options) => {
-      return new Promise((resolve, reject) => {
-        resolve({
-          ok: true,
-          json: () => {
-            return new Promise((resolve, reject) => {
-              resolve(responseData);
-            });
-          },
-        });
-      });
-    })
-  );
-  const response = await sendDataRequest("Dummy data");
+  const response = await sendDataRequest(requestData);
   return expect(response).toBe(responseData);
 });
 
 it("Should throw error if the request fails", () => {
-  vi.stubGlobal(
-    "fetch",
-    vi.fn((url, options) => {
-      return new Promise((resolve, reject) => {
-        resolve({
-          ok: false,
-          json: () => {
-            return new Promise((resolve, reject) => {
-              resolve();
-            });
-          },
-        });
+  fetchMock.mockImplementationOnce((url, options) => {
+    return new Promise((resolve, reject) => {
+      resolve({
+        ok: false,
+        json: () => {
+          return new Promise((resolve, reject) => {
+            resolve();
+          });
+        },
       });
-    })
-  );
-  return expect(sendDataRequest("Dummy data")).rejects.toThrow(HttpError);
+    });
+  });
+  return expect(sendDataRequest(requestData)).rejects.toBeInstanceOf(HttpError);
 });
 
 it("Should convert the body to a string before sending request", async () => {
-  const requestData = { key: "content" };
-  vi.stubGlobal(
-    "fetch",
-    vi.fn((url, options) => {
-      return new Promise((resolve, reject) => {
-        if (typeof options.body !== "string") {
-          return reject("Not a string.");
-        }
-        resolve({
-          ok: true,
-          json: () => {
-            return new Promise((resolve, reject) => {
-              resolve();
-            });
-          },
-        });
-      });
-    })
-  );
-
   let errorMessage;
 
   try {
